04340df016cad09bec66fbe726bb3c6b5f35e3af
Omkar Wakchaure <110598757+omkarwakchaure@users.noreply.github.com>
2025-03-06 10:27:02 +0530

    all done

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+ 
+ 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
